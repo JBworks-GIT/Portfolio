f89ce8258a8ccf707e9423fd4cc7a69cceb88aca
jai <worksome1028@gmail.com>
2024-07-31 00:05:15 +0530

    made almost everything

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,28 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hi, I'm Jai Bhatia, a passionate and dedicated developer with
+            expertise in front-end, back-end, UI & UX. With a keen eye for
+            design and a strong foundation in coding, I create seamless and
+            efficient digital experiences. My goal is to bridge the gap between
+            design and technology, ensuring both functionality and aesthetics in
+            every project.
+          </p>
+          <p className="hero--section-description">
+            I am a proud alumnus of Apeejay International School
+            Faridabad(Haryana). Presently, I'm an proud student of Lovely Professional
+            University, Phagwara.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
